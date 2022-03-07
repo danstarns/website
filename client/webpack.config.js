0686f3effa062b7642338116a3926358fb88a524
Daniel Starns <danielstarns@hotmail.com>
2022-03-07 10:49:13 +0000

    feat: add meta

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -43,6 +43,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
+      publicPath: "./img/",
       favicon: "./img/sig.svg",
       template: path.join(__dirname, "src", "index.html"),
     }),
