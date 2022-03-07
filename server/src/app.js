d9ab211110b1bc8d22938c63bd4d39a7edd98f68
Daniel Starns <danielstarns@hotmail.com>
2022-03-07 07:43:16 +0000

    refactor: remove clients path

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { HTTP_PORT, STATIC_FOLDER, CLIENTS_PATH } = require("./config");
+const { HTTP_PORT, STATIC_FOLDER } = require("./config");
 const api = require("./routers/api");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(STATIC_FOLDER));
-app.use("/clients", express.static(CLIENTS_PATH));
 app.use("/api", api);
 app.get("*", express.static(STATIC_FOLDER));
 
