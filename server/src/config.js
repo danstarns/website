577a4fedbf2c5dceb12b6611e2949033601392c8
Daniel Starns <danielstarns@hotmail.com>
2022-03-07 08:23:08 +0000

    config: add default

--- server/src/config.js
+++ server/src/config.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 
 module.exports = {
-  HTTP_PORT: process.env.HTTP_PORT,
-  STATIC_FOLDER: process.env.STATIC_FOLDER,
-  EMAIL_CC: process.env.EMAIL_CC,
-  EMAIL_SENDER_ADDRESS: process.env.EMAIL_SENDER_ADDRESS,
+  HTTP_PORT: process.env.HTTP_PORT || 4000,
+  STATIC_FOLDER: process.env.STATIC_FOLDER || "../client/dist",
+  EMAIL_CC: process.env.EMAIL_CC || "danielstarns@hotmail.com",
+  EMAIL_SENDER_ADDRESS:
+    process.env.EMAIL_SENDER_ADDRESS || "me@danielstarns.com",
   EMAIL_SENDER_PASSWORD: process.env.EMAIL_SENDER_PASSWORD,
-  EMAIL_HOST: process.env.EMAIL_HOST,
+  EMAIL_HOST: process.env.EMAIL_HOST || "smtp.gmail.com",
   DOMAIN: process.env.DOMAIN,
   GITHUB_API_TOKEN: process.env.GITHUB_API_TOKEN,
   TWITTER_API_TOKEN: process.env.TWITTER_API_TOKEN,
