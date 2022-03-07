3dda73a2dca74ec76a22017bff6e6f7ecfe220c2
Daniel Starns <danielstarns@hotmail.com>
2022-03-07 15:52:32 +0000

    accessibe: rel on links

--- client/src/components/Socials.tsx
+++ client/src/components/Socials.tsx
@@ -4,14 +4,14 @@ import { faGithub, faLinkedin, faTwitterSquare, faMedium } from "../icons";
 export function Socials() {
   return (
     <div className="flex-row justify-between">
-      <a className="m-5" about="Github" href="https://github.com/danstarns">
+      <a className="m-5" rel="Github" href="https://github.com/danstarns">
         <FontAwesomeIcon
           className="hover:text-purple-500"
           icon={faGithub}
           size="2x"
         />
       </a>
-      <a className="m-5" about="Twitter" href="https://twitter.com/dan_starns">
+      <a className="m-5" rel="Twitter" href="https://twitter.com/dan_starns">
         <FontAwesomeIcon
           className="hover:text-blue-500"
           icon={faTwitterSquare}
@@ -20,7 +20,7 @@ export function Socials() {
       </a>
       <a
         className="m-5"
-        about="Linkedin"
+        rel="Linkedin"
         href="https://www.linkedin.com/in/danielstarns/"
       >
         <FontAwesomeIcon
@@ -29,7 +29,7 @@ export function Socials() {
           size="2x"
         />
       </a>
-      <a className="m-5" about="Medium" href="https://medium.com/@danstarns">
+      <a className="m-5" rel="Medium" href="https://medium.com/@danstarns">
         <FontAwesomeIcon
           className="hover:text-green-500"
           icon={faMedium}
