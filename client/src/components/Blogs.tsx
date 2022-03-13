5540edfd3fb8ef6a9405bdcc1d375a62ddd673a9
Daniel Starns <danielstarns@hotmail.com>
2022-03-13 21:48:19 +0000

    feat: add postedAt time to blog

--- client/src/components/Blogs.tsx
+++ client/src/components/Blogs.tsx
@@ -1,9 +1,11 @@
+import { format } from "date-fns";
 import { useCallback, useEffect, useState } from "react";
 import { Section } from "./Section";
 
 interface IBlog {
   link: string;
   title: string;
+  pubDate: string;
 }
 
 export function Blogs() {
@@ -38,6 +40,17 @@ export function Blogs() {
           {blogs.map((b) => (
             <li key={b.title}>
               <a href={b.link}>{b.title}</a>
+              <p>
+                <span className="italic text-sm">
+                  -{" "}
+                  <time
+                    title={`Time Posted: ${new Date(b.pubDate).toUTCString()}`}
+                    dateTime={new Date(b.pubDate).toISOString()}
+                  >
+                    {format(new Date(b.pubDate), "h:mm a - MMM d, y")}
+                  </time>
+                </span>
+              </p>
             </li>
           ))}
         </ul>
