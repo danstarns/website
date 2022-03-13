d4277ee9ad879604bf1ec5274eca5b4f3d1f4a46
Daniel Starns <danielstarns@hotmail.com>
2022-03-13 12:07:36 +0000

    config: refactor

--- server/config.js
+++ server/config.js
@@ -1,14 +1,23 @@
 require("dotenv").config();
 
+const {
+  HTTP_PORT,
+  STATIC_FOLDER,
+  EMAIL_CC,
+  EMAIL_SENDER_ADDRESS,
+  EMAIL_SENDER_PASSWORD,
+  EMAIL_HOST,
+  GITHUB_API_TOKEN,
+  TWITTER_API_TOKEN,
+} = process.env;
+
 module.exports = {
-  HTTP_PORT: process.env.HTTP_PORT || 4000,
-  STATIC_FOLDER: process.env.STATIC_FOLDER || "../client/dist",
-  EMAIL_CC: process.env.EMAIL_CC || "danielstarns@hotmail.com",
-  EMAIL_SENDER_ADDRESS:
-    process.env.EMAIL_SENDER_ADDRESS || "me@danielstarns.com",
-  EMAIL_SENDER_PASSWORD: process.env.EMAIL_SENDER_PASSWORD,
-  EMAIL_HOST: process.env.EMAIL_HOST || "smtp.gmail.com",
-  DOMAIN: process.env.DOMAIN,
-  GITHUB_API_TOKEN: process.env.GITHUB_API_TOKEN,
-  TWITTER_API_TOKEN: process.env.TWITTER_API_TOKEN,
+  HTTP_PORT,
+  STATIC_FOLDER,
+  EMAIL_CC,
+  EMAIL_SENDER_ADDRESS,
+  EMAIL_SENDER_PASSWORD,
+  EMAIL_HOST,
+  GITHUB_API_TOKEN,
+  TWITTER_API_TOKEN,
 };
